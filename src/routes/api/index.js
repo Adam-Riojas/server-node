77942574095b01d821e07c2fa724a6426be241fa
Adam-Riojas <adam_riojas@hotmail.com>
2023-04-19 00:24:39 -0500

    added companies

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import persons from './persons'
+import companies from './companies'
 
 const router = Router()
 
@@ -8,6 +9,7 @@ router.get('/', (req, res) => {
   res.send({ msg: 'Inside API Endpoints' })
 })
 
-router.use('./persons', persons)
+router.use('/persons', persons)
+router.use('/companies', companies)
 
 export default router
