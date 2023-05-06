93a199765829895802177502b9a6cb54db9a2b2c
Adam-Riojas <adam_riojas@hotmail.com>
2023-05-05 21:32:12 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 import './helpers/dotenv'
-
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
-
-
 import logger from './helpers/logger'
 import router from './routes'
-import {notFound, errorHandler} from './helpers/errors'
-
-
+import { notFound, errorHandler } from './helpers/errors'
+const port = Number(process.env.PORT)
 const app = express()
 
-
 app.use(morgan(process.env.MORGAN_LOG))
-app.use(cors({ origin: process.env.ORIGIN, exposedHeaders: ['x-total-count', 'x-total-pages'], }),)
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    exposedHeaders: ['x-total-count', 'x-total-pages'],
+  }),
+)
 app.use(helmet())
 app.use(bodyParser.json())
 
-app.use(router)
-
+app.use('/', router)
 app.use(notFound)
 app.use(errorHandler)
-
-const port = parseInt(process.env.PORT, 10) || 3000
-
-
-
-
-app.listen(port, () => 
-    logger.info(`Server running port ${port}`))
+app.listen(port, () => {
+  logger.info(`Server running on port ${port}`)
+})
